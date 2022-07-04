be0af64bd6d7ee8ceb70aa4ff9ec11d26102840c
Wayne <106159269+WayneM37@users.noreply.github.com>
2022-07-04 11:04:55 +0300

    Dic_1 ilk sürüm

---
+++ dict_1.py
@@ -0,0 +1,29 @@
+"""This is simple dictionar. When you enter an English word it returns the Turkish \
+    translations. İf the entered word is not in our dictionary, it asks to enter\
+        the Turkish translation. Dict. data is tored in dict_2.json file. 
+     """
+
+import json
+dict_1 = {"Blue": "Mavi", "Apple": "Elma"}
+
+with open("dict_2.json", "r+", encoding="utf-8") as origin:
+    temp = json.loads(origin.read())
+    origin.close()
+ans=""
+new= ""
+while True:
+  entry = input("Please enter a word to search in dictionary: ").title()
+  if entry.isalpha() == True:
+    if entry in temp:
+        ans= temp[entry]
+        print(entry, ":", ans)
+    else:
+      print(f"The word {entry} is not in our dictionary.")
+      new = input(f"Please entter the Turkish translation of the word {entry} : ").title()
+      dict_1.update({entry:new})
+      with open("dict_2.json", "w", encoding="utf-8") as origin:
+        origin.write(json.dumps(dict_1))
+        origin.close()
+    break
+  else:
+    print("You made an incorrect entry, please try again!")
